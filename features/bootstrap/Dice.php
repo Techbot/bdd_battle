ffae1bd98d8037c84dce6721d264ebac05a635a5
techbot <djtechbot@gmail.com>
2016-03-16 21:19:00 +0100

    Empty classes. first set of behat failures :-)

---
+++ features/bootstrap/Dice.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: techbot
+ * Date: 16-3-16
+ * Time: 21:16
+ */
+
+
+
+class Dice
+{
+
+}
---
+++ features/bootstrap/Player.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: techbot
+ * Date: 16-3-16
+ * Time: 21:17
+ */
+class Player
+{
+
+}
